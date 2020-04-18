bfbe3c92a4034faca64d77984ee66511d393f989
Dharmendra Verma <sdharmendra8858@gmail.com>
2020-04-18 11:32:14 +0530

    dynamic component added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -21,6 +21,8 @@ import { Capitalise } from './recipes/capitalise.pipe';
 import { AuthComponent } from './auth/auth.component';
 import { LoadingSpinnerComponent } from './shared/loading-spinner/loading-spinner.component';
 import { AuthInterceptorSerivce } from './auth/auth-interceptor.service';
+import { AlertComponent } from './shared/alert/alert.component';
+import { PlaceholderDirective } from './shared/placeholder/placeholder.directive';
 
 @NgModule({
   declarations: [
@@ -37,7 +39,9 @@ import { AuthInterceptorSerivce } from './auth/auth-interceptor.service';
     RecipeStartComponent,
     RecipeEditComponent,
     AuthComponent,
-    LoadingSpinnerComponent
+    LoadingSpinnerComponent,
+    AlertComponent,
+    PlaceholderDirective
   ],
   imports: [
     BrowserModule,
--- src/app/auth/auth.component.ts
+++ src/app/auth/auth.component.ts
@@ -1,20 +1,34 @@
-import { Component } from "@angular/core";
+import { 
+	Component, 
+	ComponentFactoryResolver, 
+	ViewChild, 
+	OnDestroy, 
+	ViewContainerRef} from "@angular/core";
 import { NgForm } from "@angular/forms";
-import { Observable } from 'rxjs';
+import { Observable, Subscription } from 'rxjs';
 import { Router } from '@angular/router';
 
 import { AuthService, AuthResponseData } from "./auth.service";
+import { AlertComponent } from '../shared/alert/alert.component';
+import { PlaceholderDirective } from '../shared/placeholder/placeholder.directive';
 
 @Component({
   selector: "app-auth",
   templateUrl: "auth.component.html",
   styleUrls: ["./auth.component.css"],
 })
-export class AuthComponent {
-  constructor(private authService: AuthService, private router: Router) {}
+export class AuthComponent implements OnDestroy{
+  constructor(
+		private authService: AuthService, 
+		private router: Router, 
+		private viewContainerRef: ViewContainerRef,
+		private componentFactoryResolver: ComponentFactoryResolver) {}
 	isLoginMode = true;
 	isLoading  = false;
 	error: string = null;
+	private closeSub: Subscription;
+
+  @ViewChild(PlaceholderDirective, { static: true }) alertHost: PlaceholderDirective;
 
   onChangeMode() {
     this.isLoginMode = !this.isLoginMode;
@@ -29,9 +43,8 @@ export class AuthComponent {
       return;
 		}
 		const email = authData.value.email;
-		const password = authData.value.email;
+		const password = authData.value.password;
 		this.isLoading = true;
-
 		let authObs: Observable<AuthResponseData>;
 		
 		if(this.isLoginMode){
@@ -47,10 +60,35 @@ export class AuthComponent {
 			},
 			(errorRes) => {
 				this.isLoading = false;
+				this.showErrorAlert(errorRes);
 				this.error = errorRes;
 			}
 		);
 
     authData.reset();
+	}
+	
+	onHandleError(){
+		this.error = null;
+	}
+
+	private showErrorAlert(message: string){
+		const alertCmpFactory = this.componentFactoryResolver.resolveComponentFactory(AlertComponent);
+		const hostViewContainerRef = this.alertHost.viewContainerRef;
+		hostViewContainerRef.clear();
+
+		const componentRef =  hostViewContainerRef.createComponent(alertCmpFactory);
+		componentRef.instance.message = message;
+
+		this.closeSub = componentRef.instance.close.subscribe(() => {
+			this.closeSub.unsubscribe();
+			hostViewContainerRef.clear();
+		})
+	}
+
+	ngOnDestroy() {
+		if(this.closeSub){
+			this.closeSub.unsubscribe();
+		}
   }
 }
